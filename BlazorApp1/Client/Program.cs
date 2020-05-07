ca22d3d14e67f971f8f95c6582858b6440dac7c0
LunicLynx <luniclynx@outlook.com>
2020-05-08 00:24:26 +0200

    Initial

---
+++ BlazorApp1/Client/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorApp1.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddHttpClient("BlazorApp1.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorApp1.ServerAPI"));
+
+            //builder.Services.AddApiAuthorization();
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                options.ProviderOptions.Authority = "https://localhost:5001";
+                options.ProviderOptions.ClientId = "BlazorApp1.Client";
+                options.ProviderOptions.DefaultScopes.Add("BlazorApp1.ServerAPI");
+                options.ProviderOptions.RedirectUri = "https://localhost:44367/authentication/login-callback";
+                options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44367/authentication/logout-callback";
+                options.ProviderOptions.ResponseType = "code";
+                
+                options.AuthenticationPaths.RemoteRegisterPath = "https://localhost:5001/Account/Register";
+                options.AuthenticationPaths.RemoteProfilePath = "https://localhost:5001/Account/Manage";
+            });
+
+        //https://localhost:44367/_configuration/BlazorApp1.Client
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new[]
+            {
+                new ApiResource("BlazorApp1.ServerAPI", "My Demo Api")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                new Client
+                {
+                    ClientId = "BlazorApp1.Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    ClientSecrets =
+                    {
+                        new Secret("".Sha256())
+                    },
+
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "BlazorApp1.ServerAPI"
+                    },
+                    RedirectUris =
+                    {
+                        "https://localhost:44367/authentication/login-callback"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:44367/authentication/logout-callback"
+                    }
+                },
+            };
+
+    }
+}
